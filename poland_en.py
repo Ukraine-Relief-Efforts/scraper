3e44c413581569b2e93cbd0f04b9c7c6e3f77f12
vianho <silviana.h016@gmail.com>
2022-02-26 05:19:55 +0800

    added comments and avoid repeating code for reception list scraper

--- poland_en.py
+++ poland_en.py
@@ -22,7 +22,7 @@ def get_website_content():
 
 """Gets the content from a bullet points list of general information for Ukrainian citizens."""
 def get_core(content):
-  items = content.find('div', class_="editor-content").find("div").findAll("span")
+  items = content.find('div', class_="editor-content").findAll("span")
   text_arr = []
   for item in items:
     if item.find(text="RECEPTION POINT ADDRESS"):
@@ -39,37 +39,34 @@ def get_reception_points(soup):
   count = 0
 
   for item in items:
-
+    # start scraping for reception points after the title
     if item.find(text="RECEPTION POINT ADDRESS"):
       reception_list_start = True
       continue
+    # stop scraping after "what's next?"
     elif item.find(text="What next?"):
       break
 
     if reception_list_start:
       count += 1
-      # special case
+      r = Reception()
+      r.location = normalize(item.get_text(strip=True, separator=' '))
+      gmaps = item.find('a', href=True)
+      
+      if gmaps:
+        r.gmaps = gmaps['href']
+      img = item.find('img', src=True)
+
+      # first item is special because the qr and address are in the same <p> tag
       if count == 1:
-        r = Reception()
-        r.location = normalize(item.get_text(strip=True, separator=' '))
-        gmaps = item.find('a', href=True)
-        
-        if gmaps:
-          r.gmaps = gmaps['href']
-        img = item.find('img', src=True)
         if img:
           r.qr = img['src']
         recep_arr.append(r)
         continue
 
-      # normal case
+      # normal items: qr and address are in separate <p> tags
       if count % 2 == 0:
-        r = Reception()
-        r.location = normalize(item.get_text(strip=True, separator=' '))
-        gmaps = item.find('a', href=True)
-        if gmaps:
-          r.gmaps = gmaps['href']
-          recep_arr.append(r)
+        recep_arr.append(r)
       else:
         # Get from the end of array,
         img = item.find('img', src=True)
