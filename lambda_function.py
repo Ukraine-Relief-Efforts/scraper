9fd5080be2b3ac7d52ddc9376dc93268b09b2b45
Blarp Technologies <blarptechnologies@icloud.com>
2022-02-26 15:52:05 -0700

    commenting out broken stuff

--- lambda_function.py
+++ lambda_function.py
@@ -5,8 +5,8 @@ sys.path.insert(0, './deps')
 from scrapers.poland import scrape_poland_pl, scrape_poland_en
 from scrapers.poland_pl import scrape_poland_pl
 from scrapers.poland_en import scrape_poland_en
-from scrapers.hungary_hu import scrape_hungary_hu
-from scrapers.moldova_ro import scrape_moldova_ro
+# from scrapers.hungary_hu import scrape_hungary_hu
+# from scrapers.moldova_ro import scrape_moldova_ro
 
 
 def lambda_handler(event, context):
