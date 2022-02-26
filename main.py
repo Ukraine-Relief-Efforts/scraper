05e29f250c3870be0d151143cbb8e420bce7ae5f
Jordan Clare <jordan.clare.ctr@us.af.mil>
2022-02-26 12:58:01 -0500

    add Hungary scraper

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from scrapers.poland_pl import scrape_poland_pl
 from scrapers.poland_en import scrape_poland_en
 from scrapers.moldova_ro import scrape_moldova_ro
+from scrapers.hungary_hu import scrape_hungary_hu
 
 if __name__=="__main__":
-    print("Scraping Poland (PL)")
-    scrape_poland_pl()
-    print("Scraping Poland (EN)")
-    scrape_poland_en()
+    #print("Scraping Poland (PL)")
+    #scrape_poland_pl()
+    #print("Scraping Poland (EN)")
+    #scrape_poland_en()
 
-    print("Scraping Moldova (RO)")
-    scrape_moldova_ro()
+    #print("Scraping Moldova (RO)")
+    #scrape_moldova_ro()
+
+    print("Scraping Hungary (HU)")
+    scrape_hungary_hu()
     
