06367372890bda781fb152a5818e60522c509c17
SapientLion <51177639+SapientLion@users.noreply.github.com>
2022-02-27 00:11:06 +0100

    Small improvements to the main function (#32)
    * main.py: move print statements from the main function to the scrapers.
    * Implement a switch with which a specific country (scraper) can be chosen.

--- main.py
+++ main.py
@@ -2,17 +2,45 @@ from scrapers.poland import scrape_poland_pl, scrape_poland_en, scrape_poland_ua
 from scrapers.moldova_ro import scrape_moldova_ro
 from scrapers.hungary_hu import scrape_hungary_hu
 
+#
+# Country switch.
+#
+def switch_country(country):
+    country_switch = {
+        0: "ALL",
+        1: "PL_PL",
+        2: "PL_EN",
+        3: "PL_UA",
+        4: "MD_MD",
+        5: "HU_HU"
+    }
+
+    return country_switch.get(country, "ALL")
+
 if __name__=="__main__":
-    print("Scraping Poland (PL)")
-    scrape_poland_pl()
-    print("Scraping Poland (EN)")
-    scrape_poland_en()
-    print("Scraping Poand (UA)")
-    scrape_poland_ua()
+    #
+    # Default value which is 'ALL'.
+    #
+    country = 0
 
-    print("Scraping Moldova (RO)")
-    scrape_moldova_ro()
+    if switch_country(country) == "ALL":
+        scrape_poland_pl()
+        scrape_poland_en()
+        scrape_poland_ua()
+        scrape_moldova_ro()
+        scrape_hungary_hu()
 
-    print("Scraping Hungary (HU)")
-    scrape_hungary_hu()
-    
+    if switch_country(country) == "PL_PL":
+        scrape_poland_pl()
+
+    if switch_country(country) == "PL_EN":
+        scrape_poland_en()
+
+    if switch_country(country) == "PLA_UA":
+        scrape_poland_ua()
+
+    if switch_country(country) == "MD_MD":
+        scrape_moldova_ro()
+
+    if switch_country(country) == "HU_HU":
+        scrape_hungary_hu()
