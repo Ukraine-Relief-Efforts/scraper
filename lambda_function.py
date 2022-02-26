dfd13f1d71275f92ccce4b2db8925a852af0f919
Blarp Technologies <blarptechnologies@icloud.com>
2022-02-26 14:35:30 -0700

    Scrape poland pl works

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,7 @@ import sys
 sys.path.insert(0, './deps')
 
 # from moldova_ro import scrape_moldova_ro
-from poland_pl import scrape_poland_pl
+from scrapers.poland_pl import scrape_poland_pl
 
 
 def lambda_handler(event, context):
