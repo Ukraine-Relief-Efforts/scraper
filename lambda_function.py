6f21cd8407d352ada43afac5268489f5d871444f
Blarp Technologies <blarptechnologies@icloud.com>
2022-02-26 17:55:41 -0700

    I'm dumb

--- lambda_function.py
+++ lambda_function.py
@@ -5,7 +5,7 @@ sys.path.insert(0, './deps')
 from scrapers.poland import scrape_poland_pl, scrape_poland_en
 from scrapers.poland_pl import scrape_poland_pl
 from scrapers.poland_en import scrape_poland_en
-# from scrapers.hungary_hu import scrape_hungary_hu
+from scrapers.hungary_hu import scrape_hungary_hu
 # from scrapers.moldova_ro import scrape_moldova_ro
 
 
@@ -17,8 +17,8 @@ def lambda_handler(event, context):
     print("Scraping Poland (EN)")
     scrape_poland_en()
 
-    # print("Scraping Hungary (HU)")
-    # scrape_hungary_hu()
+    print("Scraping Hungary (HU)")
+    scrape_hungary_hu()
 
     # print("Scraping Moldova (RO)")
     # scrape_moldova_ro()
