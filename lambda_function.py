073984725b5d35a1565e7d787420453c6a243cc7
Blarp Technologies <blarptechnologies@icloud.com>
2022-02-27 09:22:22 -0700

    Added 5th scraper

--- lambda_function.py
+++ lambda_function.py
@@ -2,8 +2,7 @@ import sys
 
 sys.path.insert(0, './deps')
 
-from scrapers.poland import scrape_poland_pl, scrape_poland_en
-from scrapers.poland_en import scrape_poland_en
+from scrapers.poland import scrape_poland_pl, scrape_poland_en, scrape_poland_ua
 from scrapers.hungary_hu import scrape_hungary_hu
 from scrapers.moldova_ro import scrape_moldova_ro
 
@@ -12,5 +11,6 @@ def lambda_handler(event, context):
     """Call all scrapers."""
     scrape_poland_pl()
     scrape_poland_en()
+    scrape_poland_ua()
     scrape_hungary_hu()
     scrape_moldova_ro()
