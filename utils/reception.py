52002148e3120319f3e7cb6d6899888a0a1ee433
Bradley Reynolds <bradley.reynolds@darbia.dev>
2022-02-26 21:33:03 -0600

    Revert "Testing hombrew KML parser"
    This reverts commit 2d19d6b4568ff507c0082f858595133db72e6682.

--- utils/reception.py
+++ utils/reception.py
@@ -1,13 +1,50 @@
-from dataclasses import dataclass
-
-
-@dataclass
 class Reception:
-    name: str = ""
-    address: str = ""
-    qr: str = ""
-    lat: str = ""
-    lon: str = ""
+  def __init__(self):
+    self._name = ""
+    self._address = ""
+    self._qr = ""
+    self._lat = ""
+    self._lon = ""
+    
+  def __str__(self):
+    return self._name
+  
+  @property
+  def name(self):
+    return self._name
 
-    def __str__(self):
-        return self.name
+  @name.setter
+  def name(self, name):
+    self._name = name
+  
+  @property
+  def address(self):
+    return self._address
+
+  @address.setter
+  def address(self, loc):
+    self._address = loc
+  
+  @property
+  def qr(self):
+    return self._qr
+
+  @qr.setter
+  def qr(self, qr):
+    self._qr = qr
+    
+  @property
+  def lat(self):
+    return self._lat
+
+  @lat.setter
+  def lat(self, lat):
+    self._lat = lat
+
+  @property
+  def lon(self):
+    return self._lon
+
+  @lon.setter
+  def lon(self, lon):
+    self._lon = lon
