d5bdbeefb39140a48d42f01e7ddd3d40a6d5a86c
Blarp Technologies <blarptechnologies@icloud.com>
2022-02-27 22:57:08 -0700

    Fixed merge conflict

--- integ_test/test_scrapers.py
+++ integ_test/test_scrapers.py
@@ -14,14 +14,11 @@ def put_item():
     with patch("utils.dynamo.client.put_item", MagicMock(name="put_item")) as put_item:
         yield put_item
 
-<<<<<<< HEAD
 @pytest.fixture(autouse=True)
 def get_item():
     """Mock dynamo.... AGAIN!??!!?! What is this garbo..."""
     with patch('utils.dynamo.client.get_item', MagicMock(name='get_item')) as get_item:
         yield get_item
-=======
->>>>>>> master
 
 @pytest.fixture()
 def check_common(put_item):
