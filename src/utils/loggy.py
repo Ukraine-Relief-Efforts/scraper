8afd2da820685c8fc8a0b0960e7f4997c4bda7ed
Blarp Technologies <blarptechnologies@icloud.com>
2022-03-06 12:15:33 -0700

    Added print statements to check the paths used in logging to sumo

--- src/utils/loggy.py
+++ src/utils/loggy.py
@@ -1,4 +1,5 @@
 """Functionality related to logging."""
+from asyncore import file_dispatcher
 import json
 import logging
 from pathlib import Path
@@ -9,6 +10,12 @@ def init_logging():
     base_dir = Path(__file__).parent
     sumo_secrets_path = base_dir / ".sumo-code.json"
     log_config_path = base_dir / "logConfig.json"
+    sumo_secrets = ""
+    log_config = ""
+
+    print("Sumo Secrets Path: " + sumo_secrets_path)
+    print("Log Config Path: " + log_config_path)
+    
     try:
         sumo_secrets = json.loads(sumo_secrets_path.read_text())
         log_config = json.loads(log_config_path.read_text())
