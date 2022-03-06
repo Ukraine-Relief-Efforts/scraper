a28829c7a33e01a821a4f7562cda01d72f3508cd
Blarp Technologies <blarptechnologies@icloud.com>
2022-03-06 12:17:49 -0700

    Cleanup

--- src/utils/loggy.py
+++ src/utils/loggy.py
@@ -1,5 +1,4 @@
 """Functionality related to logging."""
-from asyncore import file_dispatcher
 import json
 import logging
 from pathlib import Path
@@ -10,8 +9,6 @@ def init_logging():
     base_dir = Path(__file__).parent
     sumo_secrets_path = base_dir / ".sumo-code.json"
     log_config_path = base_dir / "logConfig.json"
-    sumo_secrets = ""
-    log_config = ""
 
     print("Sumo Secrets Path: " + sumo_secrets_path)
     print("Log Config Path: " + log_config_path)
