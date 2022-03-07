7aa527981a7e9243855e25bebbce9606af24fb75
andrewbowen19 <atbowen@gmail.com>
2022-03-07 08:04:31 -0500

    Add len check to pl reception pts

--- unit_test/test_poland.py
+++ unit_test/test_poland.py
@@ -11,9 +11,10 @@ import requests
 sys.path.insert(0, os.path.join("..", "src"))
 sys.path.insert(1, os.path.join("..", "src", "utils"))
 from scrapers.poland import PolandScraper, POLAND_PL_RECEPTION_URL, POLAND_PL_URL
+from utils.utils import get_reception_points, get_website_content
 
-html = requests.get(POLAND_PL_RECEPTION_URL)
-soup = BeautifulSoup(html.text, features="html.parser")
+# html = requests.get(POLAND_PL_RECEPTION_URL)
+# soup = BeautifulSoup(html.text, features="html.parser")
 
 
 @pytest.fixture
@@ -24,24 +25,29 @@ def pls():
 
 @pytest.fixture
 def html_soup():
-    html = requests.get(POLAND_PL_RECEPTION_URL)
-    soup = BeautifulSoup(html.text)  # , features="html.parser")
-    return html, soup
+    soup = get_website_content(POLAND_PL_URL)
+    return soup
 
 
 @pytest.fixture(params=["pl", "en"])
 def test_get_core(pls, request):
     content = html.text
     text_arr = pls.get_core(content, request.param)
+
+    # Check text array is not empty list
     assert isinstance(text_arr, list)
+    assert text_arr
 
 
-def test_get_reception_points_en(pls, html_soup):
-    assert isinstance(pls.get_reception_points_pl(soup), list)
+# Website broken -- don't test
+# def test_get_reception_points_en(pls, html_soup):
+#     assert isinstance(pls.get_reception_points_pl(soup), list)
 
 
-def test_get_reception_points_pl(pls):
-    assert isinstance(pls.get_reception_points_en(soup), list)
+def test_get_reception_points_pl(pls, html_soup):
+    pts = pls.get_reception_points_en(html_soup)
+    assert isinstance(pts, list)
+    assert pts
 
 
 def test_scrape(pls):
