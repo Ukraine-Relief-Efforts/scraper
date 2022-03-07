257418c4b80375fd6ed16539fd58ddeaf565a331
andrewbowen19 <atbowen@gmail.com>
2022-03-06 21:12:22 -0500

    Add poland test scraper fixtures

--- unit_test/test_poland.py
+++ unit_test/test_poland.py
@@ -9,23 +9,52 @@ import requests
 
 # Import poland scraper
 sys.path.insert(0, os.path.join("..", "src"))
-from scrapers import poland
+from scrapers.poland import PolandScraper, POLAND_PL_RECEPTION_URL, POLAND_PL_URL
+
+html = requests.get(POLAND_PL_RECEPTION_URL)
+soup = BeautifulSoup(html.text, features="html.parser")
 
 
-def test_get_reception_points_en():
-    html = requests.get(poland.POLAND_PL_RECEPTION_URL)
-    soup = BeautifulSoup(html.text)
-    assert isinstance(poland.PolandScraper().get_reception_points_pl(soup), list)
+@pytest.fixture
+def pls():
+    pls = PolandScraper()
+    return pls
 
 
-def test_get_reception_points_pl():
-    html = requests.get(poland.POLAND_PL_RECEPTION_URL)
-    soup = BeautifulSoup(html.text)
-    assert isinstance(poland.PolandScraper().get_reception_points_en(soup), list)
+@pytest.fixture
+def html_soup():
+    html = requests.get(POLAND_PL_RECEPTION_URL)
+    soup = BeautifulSoup(html.text)  # , features="html.parser")
+    return html, soup
 
 
-# def test_scrape():
-#     assert poland.PolandScraper().scrape() is None
+@pytest.fixture(params=["pl", "en"])
+def test_get_core(pls, request):
+    content = html.text
+    text_arr = pls.get_core(content, request.param)
+    assert isinstance(text_arr, list)
+
+
+def test_get_reception_points_en(pls, html_soup):
+    assert isinstance(pls.get_reception_points_pl(soup), list)
+
+
+def test_get_reception_points_pl(pls):
+    assert isinstance(pls.get_reception_points_en(soup), list)
+
+
+def test_scrape(pls):
+    assert pls.scrape() is None
+
+
+def test_scrape_poland_pl():
+    pass
+
+
+@pytest.fixture(params=["pl", "en", "ua"])
+def test_scrape_poland(pls, request):
+    pass
+
 
 # From test_dynamo.py
 # @pytest.fixture(autouse=True)
@@ -37,11 +66,17 @@ def test_get_reception_points_pl():
 # @pytest.fixture(autouse=True)
 # def test_scrape_poland(put_item):
 #     url = poland.POLAND_PL_URL
-#     p = poland.PolandScraper().scrape_poland(url, "en", "")
+#     p = PolandScraper().scrape_poland(url, "en", "")
 #     assert isinstance(p, list)
 
 
 # TODO:
 # - Figure out dynmo write bypass for testing (still test methods)
-# - Write tests for rest of poland.PolandScraper
+# - Write tests for rest of PolandScraper
 # - Write tests for other country scrapers
+
+if __name__ == "__main__":
+    p = PolandScraper()
+    print(soup)
+    recep_arr = p.get_reception_points_en(soup)
+    print(recep_arr)
