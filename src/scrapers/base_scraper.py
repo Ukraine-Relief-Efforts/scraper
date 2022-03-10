286b1e3ab37c2b5524d2488057119d56d598972b
Bradley Reynolds <bradley.reynolds@darbia.dev>
2022-03-09 22:35:14 -0600

    Adding Sphinx

--- src/scrapers/base_scraper.py
+++ src/scrapers/base_scraper.py
@@ -1,7 +1,12 @@
+"""Scraper base class"""
+
 from abc import ABC, abstractmethod
 
 
 class BaseScraper(ABC):
+    """An abstract base class for scrapers"""
+
     @abstractmethod
     def scrape(self):
+        """The abstract method."""
         pass
