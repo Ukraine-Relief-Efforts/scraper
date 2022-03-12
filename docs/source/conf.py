6eb97e772af1cccaec18af46db4b56e5254296b4
Bradley Reynolds <bradley.reynolds@darbia.dev>
2022-03-12 11:37:36 -0600

    Update docs to use new location for metadata

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,31 +1,30 @@
+#!/usr/bin/env python
+"""
+Sphinx configuration
+"""
+
 # Configuration file for the Sphinx documentation builder.
-#
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-
 import os
 import sys
 
+import toml
+
 sys.path.insert(0, os.path.abspath("../.."))
 
-# -- Project information -----------------------------------------------------
-
-project = "scraper"
-copyright = "2022, TechFor_"
-author = "TechFor_"
-git_url = "https://github.com/Ukraine-Relief-Efforts/scraper"
-
-# -- General configuration ---------------------------------------------------
+project_config = toml.load("../../pyproject.toml")
+project: str = project_config["project"]["name"]
+release: str = project_config["project"]["version"]
+git_url: str = project_config["project"]["urls"]["repository"]
+copyright: str = project_config["tool"]["sphinx"]["copyright"]
+author: str = project_config["tool"]["sphinx"]["author"]
+api_dir: str = project_config["tool"]["sphinx"]["api_dir"]
 
 # Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# extensions coming with Sphinx (named "sphinx.ext.*") or your custom
 # ones.
 extensions = [
     "sphinx.ext.autodoc",
@@ -39,7 +38,7 @@ extensions = [
     "sphinxcontrib.autoprogram",
 ]
 
-apidoc_module_dir: str = f"../../src"
+apidoc_module_dir: str = f"../../{api_dir}"
 
 autoapi_type: str = "python"
 autoapi_dirs: list[str] = [apidoc_module_dir]
@@ -49,24 +48,23 @@ autosummary_generate: bool = True
 add_module_names: bool = True
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ["_templates"]
+templates_path: list[str] = ["_templates"]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns: list[str] = ["_build", "Thumbs.db", ".DS_Store"]
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-#
 html_theme: str = "sphinx_rtd_theme"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+html_static_path: list[str] = ["_static"]
 
 
 def linkcode_resolve(domain, info):
