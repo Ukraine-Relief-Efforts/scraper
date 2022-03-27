7666ee9923613ef1f89e4602294fc823cd497eea
Will Hartzell-Baird <WillDHB@gmail.com>
2022-03-26 20:28:41 -0400

    Implement geocoding

---
+++ unit_test/test_geo.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from utils.geo import address_to_coords, _get_key_path
+
+# https://developer.mapquest.com/documentation/open/geocoding-api/address/get/
+
+
+def address_to_coords_without_key_returns_none(fs):
+    result = address_to_coords("asdf")
+    assert result is None
+
+
+def test_address_to_coords(mrget, fs):
+    key_path = _get_key_path()
+    key_path.parent.mkdir(parents=True, exist_ok=True)
+    key_path.write_text("my-key")
+    mrget().json.return_value = {
+        "results": [{"locations": [{"latLng": {"lat": 4, "lng": 2}}]}]
+    }
+    result = address_to_coords(
+        "Suchodolski Palace Communal Culture and Tourism Center, ul. Parkowa 5, 22-175 Dorohusk - housing estate"
+    )
+    assert result == (4, 2)
